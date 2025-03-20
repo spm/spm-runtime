de7226658463fcca23bdfb3c0f566f5520ec4ff6
balbasty <yael.balbastre@gmail.com>
2025-03-20 15:29:20 +0000

    Fix import

--- spm_runtime/__init__.py
+++ spm_runtime/__init__.py
@@ -6,9 +6,9 @@ __all__ = [
 import matlab_runtime
 
 from ._version import __version__, __matlab_release__
-from . import _spm_compiled
+from . import _spm
 
 matlab_runtime.init(__matlab_release__, install_if_missing=True)
 
-_deployed = matlab_runtime.import_deployed(_spm_compiled)
+_deployed = matlab_runtime.import_deployed(_spm)
 endpoint = _deployed.mpython_endpoint
